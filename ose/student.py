a25e3f28fede35d98d207f0235643524508545fa
Arnaud Rachez <arnaud.rachez@gmail.com>
2018-07-26 14:27:40 +0200

    ENH docstring update

--- ose/student.py
+++ ose/student.py
@@ -53,12 +53,23 @@ class PoissonStudent(Student):
         return s
 
     def add(self, statement):
+        """
+        Add the xAPI statement the history of the student.
+
+        Parameters
+        ----------
+        statement: Statement
+            The xAPI statement to add to the model
+        """
         actor, name = statement['actor'], self.name
         if actor != name:
             raise('Statement with actor %s assigned to %s' % (actor, name))
         self.timestamps.append(statement['timestamp'])
 
     def fit(self):
+        """
+        Builds a Bayesian Network from the history of xAPI statements
+        """
         self.timestamps = sorted(self.timestamps)
         self.dt = reduce((lambda x, y: y - x), self.timestamps)
         self.expT = self.dt
