14ed3780a6dc7af2eb330abfce556a3b74326403
Arnaud Rachez <arnaud.rachez@gmail.com>
2018-07-30 15:56:41 +0200

    ENH Draft of fit method.

--- ose/main.py
+++ ose/main.py
@@ -1,6 +1,8 @@
-from environment import Environment, load_json_statements
+from pymc import Uniform, MCMC
+from pylab import hist, show
+
+from environment import Environment
 from student import PoissonStudent
-from pymc import Uniform
 
 
 def main():
@@ -16,8 +18,14 @@ def main():
     lam = Uniform('lam', lower=0, upper=1)
     students = [PoissonStudent(name=name, lam=lam) for name in student_names]
     env = Environment(students, statements)
-#     env.fit()
-#     env.show()
+    params = [lam]
+    for s in students:
+        params.extend(s.params)
+    m = MCMC(params)
+    m.sample(iter=10000, burn=1000, thin=10)
+    hist(m.trace('lambda_david')[:])
+    show()
+
 
 if __name__ == '__main__':
     main()
