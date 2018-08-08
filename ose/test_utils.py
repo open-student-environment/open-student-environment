e5451a384692ee36b7176269e0afb8486006134d
Arnaud Rachez <arnaud.rachez@gmail.com>
2018-08-08 14:35:36 +0200

    issue #25 - graph of students per group * ENH new structure for the graph of agents

--- ose/test_utils.py
+++ ose/test_utils.py
@@ -2,7 +2,7 @@ import os
 
 import unittest
 
-from .utils import load_agent_data, build_graph_from_data, graph2gephi
+from .utils import load_agent_data, get_agents_graph, graph2gephi
 
 
 agents = """{"uuid":"a","uai":null,"role":"user:enseignant","isEphemeral":0,"schoolLevels":[],"organizations":[{"id":1,"label":"Ecole du chemin","type":"ecole"},{"id":2,"label":"Classe de CP","type":"classe"},{"id":3,"label":"Enseignant Duchmol","type":"classe"},{"id":5480,"label":"L3-Anglais","type":"groupe"},{"id":17177,"label":"Groupe 2","type":"groupe"},{"id":19272,"label":"Les CM1","type":"groupe"},{"id":30207,"label":"Groupe VERT","type":"groupe"},{"id":30367,"label":"GROUPE B","type":"groupe"}],"isolution":"brneac3"}
@@ -38,14 +38,19 @@ class Test(unittest.TestCase):
 
     def test_build_graph_from_data(self):
         data = load_agent_data(self.filename)
-        nodes, adjancy = build_graph_from_data(data)
-        self.assertEqual(nodes, {'b', 'i', 'e', 'd', 'a', 'f', 'g', 'h', 'c'})
-        # TODO: Add adjancy test
+        nodes, adjancy = get_agents_graph(data)
+        agents = {
+            'a', 'b', 1, 3, 5480, 'c', 2, 'd', 'e', '0951099D', 'f',
+            '0060138T', 'g', 'h', 'i'
+        }
+        self.assertEqual(nodes.keys(), agents)
+        self.assertEqual(set(nodes.keys()).difference(set(adjancy.keys())),
+                         {'d', 'b', 'c', 'h'})
 
     def test_graph2gephi(self):
         data = load_agent_data(self.filename)
-        nodes, adjancy = build_graph_from_data(data)
-        graph2gephi(nodes, adjancy, filename='test-output.csv')
+        nodes, adjancy = get_agents_graph(data)
+        graph2gephi(nodes, adjancy, filename='../data/test-output.csv')
 
 if __name__ == "__main__":
     unittest.main()
--- ose/utils.py
+++ ose/utils.py
@@ -28,45 +28,37 @@ def load_agent_data(filename):
     return data
 
 
-def build_graph_from_data(data):
+def get_agents_graph(data):
     """
-    Returns the adjancy graph of the actors (students, teachers, school)
+    Extracts adjancy information from the list of agents
 
     Arguments
     ---------
-    filename: str
+    data: list[Agents]
         Agents datastructure
 
     Return
-    ______
-    nodes: list[str]
-        List of agent's names
-
-    adjancy: dict(str: [Agents])
-        Adjancy lists indexed by agent name (agent_names -> agents)
+    ------
+    adjancy: dict [str -> str]
     """
-    groups = defaultdict(list)
+    agents = defaultdict(dict)
+    adjancy = defaultdict(list)
     for user in data:
         username = user['uuid']
         role = user['role']
+        agents[username] = role
+        if role == 'user:eleve':
+            for g in user['organizations']:
+                adjancy[g['id']].append(username)
+                agents[g['id']] = 'group'
         if role == 'user:enseignant':
             for g in user['organizations']:
-                groups[g['id']].append(username)
-    adjancy = defaultdict(set)
-
-    for user in data:
-        username = user['uuid']
-        role = user['role']
-        for g in user['organizations']:
-            for parent in groups[g['id']]:
-                adjancy[parent].add(username)
-        if role == 'user:enseignant':
-            if user['uai'] is not None:
-                adjancy[user['uai'].replace('.', ' ')].add(username)
-
-    nodes = set(user['uuid'] for user in data)
-
-    return nodes, adjancy
+                adjancy[username].append(g['id'])
+            if user['uai']is not None:
+                school = user['uai'].replace(',', '|')
+                adjancy[school].append(username)
+                agents[school] = 'school'
+    return agents, adjancy
 
 
 def graph2gephi(nodes, adjancy, filename='test.gdf'):
@@ -82,18 +74,10 @@ def graph2gephi(nodes, adjancy, filename='test.gdf'):
         Adjancy lists indexed by agent name (agent_names -> agents)
     """
     with open(filename, 'w') as f:
-        f.write('nodedef> name VARCHAR\n')
-        for node in nodes:
-            f.write("{}\n".format(node))
+        f.write('nodedef> name VARCHAR, role VARCHAR\n')
+        for name, role in nodes.items():
+            f.write("{}, {}\n".format(name, role))
         f.write('edgedef> node1 VARCHAR, node2 VARCHAR, weight DOUBLE\n')
         for parent, children in list(adjancy.items()):
             for child in children:
                 f.write("{}, {}, {}\n".format(parent, child, 1))
-
-
-if __name__ == "__main__":
-    base_path = os.path.abspath(
-        os.path.join(os.path.dirname(__file__), os.pardir))
-    filename = os.path.join(base_path, 'data/accounts-brneac3-0-20180630.json')
-    data = load_agent_data(filename)
-    print(data[0])
